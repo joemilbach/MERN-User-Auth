79d6599323626b0e6fd74e70da20a7731cd19fd9
Joe Milbach <jmilbach@cws.net>
2020-07-09 08:39:00 -0500

    Finish basic backend scripts

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,6 +1,7 @@
 const router = require("express").Router()
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
+const auth = require("../middleware/auth")
 const User = require("../models/userModel")
 
 router.post("/register", async (req, res) => {
@@ -11,16 +12,16 @@ router.post("/register", async (req, res) => {
       return res.status(400).json({msg: "Missing required field."})
     }
     if(password.length < 5) {
-      return res.status(400).json({msg: "Minimum of 5 character in password."})
+      return res.status(400).json({msg: "Minimum of 5 character in password"})
     }
     if(password !== passwordCheck) {
-      return res.status(400).json({msg: "Password fields do not match."})
+      return res.status(400).json({msg: "Password fields do not match"})
     }
 
     const existingUser = await User.findOne({ email: email })
 
     if(existingUser) {
-      return res.status(400).json({msg: "Email already exists."})
+      return res.status(400).json({msg: "Email already exists"})
     }
 
     if(!displayName) {
@@ -47,17 +48,17 @@ router.post("/login", async (req, res) => {
     const { email, password } = req.body
 
     if(!email || !password) {
-      return res.status(400).json({msg: "Enter email & password."})
+      return res.status(400).json({msg: "Enter email & password"})
     }
 
     const user = await User.findOne({ email: email })
     if(!user) {
-      return res.status(400).json({msg: "Email not found."})
+      return res.status(400).json({msg: "Account not found"})
     }
 
     const passwordMatch = await bcrypt.compare(password, user.password)
     if(!passwordMatch) {
-      return res.status(400).json({msg: "Email and/or password incorrect."})
+      return res.status(400).json({msg: "Email and/or password incorrect"})
     }
 
     const token = jwt.sign({
@@ -72,7 +73,38 @@ router.post("/login", async (req, res) => {
         email: user.email,
       }
     })
+  } catch (error) {
+    res.status(500).json({error: error.message})
+  }
+})
 
+router.delete("/delete", auth, async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.user)
+    res.json(deletedUser)
+  } catch (error) {
+    res.status(500).json({error: error.message})
+  }
+})
+
+router.post("/validToken", async (req, res) => {
+  try {
+    const token = req.header("x-auth-token")
+    if(!token) {
+      return res.json(false)
+    }
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET)
+    if(!verified) {
+      return res.json(false)
+    }
+
+    const user = await User.findById(verified.id)
+    if(!user) {
+      return res.json(false)
+    }
+
+    return res.json(true)
   } catch (error) {
     res.status(500).json({error: error.message})
   }
