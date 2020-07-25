03cac70ab1391426efdd83f36ae3e4c8df11e602
Joe Milbach <jmilbach@cws.net>
2020-07-24 21:37:39 -0500

    Moved delete account SA user edit

--- client/src/pages/UserAccountsPage.js
+++ client/src/pages/UserAccountsPage.js
@@ -72,7 +72,7 @@ function UserAccountsPage() {
   }, [users, selectedUser.userKey]);
 
   function handleSelectUser(e) {
-    if (typeof e.target.id !== "undefined") {
+    if (e.target.id !== "") {
       // Save user index and ID
       const userKey = e.target.id;
       setSelectedUser((selectedUser) => ({
@@ -123,11 +123,25 @@ function UserAccountsPage() {
             {selectedUser.id ? (
               <>
                 <ListGroup.Item variant="info">
-                  <h2 className="h5">
-                    Editting:
-                    <small>
-                      {" " + selectedUser.username + " - " + selectedUser.email}
-                    </small>
+                  <h2 className="h4 d-flex justify-content-between align-items-center">
+                    <span>
+                      Editting:
+                      <small>
+                        {" " +
+                          selectedUser.username +
+                          " - " +
+                          selectedUser.email}
+                      </small>
+                    </span>
+                    <Button
+                      variant="outline-info"
+                      size="sm"
+                      className="d-flex align-items-center py-1"
+                      onClick={() => handleDeleteUser(selectedUser.id)}
+                    >
+                      <ExclamationTriangleFill className="mr-2" />
+                      <small>Delete Account</small>
+                    </Button>
                   </h2>
                 </ListGroup.Item>
                 <ListGroup.Item
@@ -142,7 +156,7 @@ function UserAccountsPage() {
             ) : (
               <>
                 <ListGroup.Item variant="info">
-                  <h2 className="h5">Current Users</h2>
+                  <h2 className="h4">Current Users</h2>
                 </ListGroup.Item>
                 {users.items.map((user, index) => (
                   <ListGroup.Item
@@ -163,133 +177,123 @@ function UserAccountsPage() {
       </Col>
 
       {selectedUser.id && (
-        <Form
-          className="col-md-8 col-lg-6 pt-2 mx-auto"
-          name="form"
-          onSubmit={handleSubmit}
-        >
-          {alert.message && (
-            <div className={`alert ${alert.type}`}>{alert.message}</div>
-          )}
-          <Form.Group className="mb-3" controlId="email">
-            <Form.Label>Email</Form.Label>
-            <InputGroup>
-              <InputGroup.Text>{selectedUser.email}</InputGroup.Text>
-              <Form.Control
-                type="text"
-                placeholder="johndoe@domain.com"
-                value={user.email}
-                onChange={handleChange}
-              />
-            </InputGroup>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="username">
-            <Form.Label>Username</Form.Label>
-            <InputGroup>
-              <InputGroup.Text>{selectedUser.username}</InputGroup.Text>
-              <Form.Control
-                type="text"
-                placeholder="John Doe"
-                value={user.username}
-                onChange={handleChange}
-              />
-            </InputGroup>
-          </Form.Group>
-          <Row className="mb-3">
-            <Form.Text className="col-12 mt-2 mb-3 d-none">
-              <em>
-                "Password" must be six characters or longer and must match
-                "Confirm Password".
-              </em>
-            </Form.Text>
-            <Form.Group controlId="password" className="col-md-6 mb-3">
-              <Form.Label className="w-100 d-flex justify-content-between align-items-center">
-                Password*
-                <OverlayTrigger
-                  placement="top"
-                  target="tooltip-password"
-                  overlay={
-                    <Tooltip id="tooltip-password">
-                      Six characters or longer
-                    </Tooltip>
-                  }
+        <>
+          <Form
+            className="col-md-8 col-lg-6 pt-2 mx-auto"
+            name="form"
+            onSubmit={handleSubmit}
+          >
+            {alert.message && (
+              <div className={`alert ${alert.type}`}>{alert.message}</div>
+            )}
+            <Form.Group className="mb-3" controlId="email">
+              <Form.Label>Email</Form.Label>
+              <InputGroup>
+                <InputGroup.Text>{selectedUser.email}</InputGroup.Text>
+                <Form.Control
+                  type="text"
+                  placeholder="johndoe@domain.com"
+                  value={user.email}
+                  onChange={handleChange}
+                />
+              </InputGroup>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="username">
+              <Form.Label>Username</Form.Label>
+              <InputGroup>
+                <InputGroup.Text>{selectedUser.username}</InputGroup.Text>
+                <Form.Control
+                  type="text"
+                  placeholder="John Doe"
+                  value={user.username}
+                  onChange={handleChange}
+                />
+              </InputGroup>
+            </Form.Group>
+            <Row className="mb-3">
+              <Form.Text className="col-12 mt-2 mb-3 d-none">
+                <em>
+                  "Password" must be six characters or longer and must match
+                  "Confirm Password".
+                </em>
+              </Form.Text>
+              <Form.Group controlId="password" className="col-md-6 mb-3">
+                <Form.Label className="w-100 d-flex justify-content-between align-items-center">
+                  Password*
+                  <OverlayTrigger
+                    placement="top"
+                    target="tooltip-password"
+                    overlay={
+                      <Tooltip id="tooltip-password">
+                        Six characters or longer
+                      </Tooltip>
+                    }
+                  >
+                    <InfoCircleFill className="text-info mr-2" />
+                  </OverlayTrigger>
+                </Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="******"
+                  value={user.password}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+              <Form.Group controlId="passwordConfirm" className="col-md-6 mb-3">
+                <Form.Label className="w-100 d-flex justify-content-between align-items-center">
+                  Confirm Password*
+                  <OverlayTrigger
+                    placement="top"
+                    target="tooltip-passwordConfirm"
+                    overlay={
+                      <Tooltip id="tooltip-passwordConfirm">
+                        Must match "Password"
+                      </Tooltip>
+                    }
+                  >
+                    <InfoCircleFill className="text-info mr-2" />
+                  </OverlayTrigger>
+                </Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="******"
+                  value={user.passwordConfirm}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+              <Form.Group controlId="role" className="col-md-6">
+                <Form.Label>Account Type</Form.Label>
+                <Form.Control
+                  custom
+                  as="select"
+                  className="form-select"
+                  value={user.role}
+                  onChange={handleChange}
                 >
-                  <InfoCircleFill className="text-info mr-2" />
-                </OverlayTrigger>
-              </Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="******"
-                value={user.password}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            <Form.Group controlId="passwordConfirm" className="col-md-6 mb-3">
-              <Form.Label className="w-100 d-flex justify-content-between align-items-center">
-                Confirm Password*
-                <OverlayTrigger
-                  placement="top"
-                  target="tooltip-passwordConfirm"
-                  overlay={
-                    <Tooltip id="tooltip-passwordConfirm">
-                      Must match "Password"
-                    </Tooltip>
-                  }
-                >
-                  <InfoCircleFill className="text-info mr-2" />
-                </OverlayTrigger>
-              </Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="******"
-                value={user.passwordConfirm}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            <Form.Group controlId="role" className="col-md-6">
-              <Form.Label>Account Type</Form.Label>
-              <Form.Control
-                custom
-                as="select"
-                className="form-select"
-                value={user.role}
-                onChange={handleChange}
-              >
-                <option defaultValue>Please make a selection...</option>
-                <option value="U">Standard User</option>
-                <option value="A">Admin</option>
-                <option value="SA">Super Admin</option>
-              </Form.Control>
-            </Form.Group>
-
-            <Form.Group className="col-md-6 d-flex">
+                  <option defaultValue>Please make a selection...</option>
+                  <option value="U">Standard User</option>
+                  <option value="A">Admin</option>
+                  <option value="SA">Super Admin</option>
+                </Form.Control>
+              </Form.Group>
+            </Row>
+            <Form.Group className="d-flex justify-content-between align-items-center pt-2">
               <Button
-                variant="outline-danger"
-                className="align-self-end"
-                block
-                onClick={() => handleDeleteUser(selectedUser.id)}
+                id="userListReset"
+                variant="outline-primary"
+                onClick={(e) => handleSelectUser(e)}
               >
-                <ExclamationTriangleFill className="mr-2" />
-                Delete Account
+                Back to User List
+              </Button>
+              <Button variant="warning" className="text-white" type="submit">
+                {updating && (
+                  <span className="spinner-border spinner-border-sm mr-1"></span>
+                )}
+                Edit Settings
               </Button>
             </Form.Group>
-          </Row>
-          <Form.Group className="d-flex justify-content-between align-items-center pt-2">
-            <Button
-              id="userListReset"
-              variant="outline-primary"
-              onClick={(e) => handleSelectUser(e)}
-            >
-              Back to User List
-            </Button>
-            <Button variant="warning" className="text-white" type="submit">
-              {updating && (
-                <span className="spinner-border spinner-border-sm mr-1"></span>
-              )}
-              Edit Settings
-            </Button>
-          </Form.Group>
-        </Form>
+          </Form>
+        </>
       )}
     </Container>
   );
